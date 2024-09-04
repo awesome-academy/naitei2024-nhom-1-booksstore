e62b47ca1cb26d87b49d943f05f1d68b58f7fd64
Giaanh0509 <ngotthe0509@gmail.com>
2024-09-04 15:08:55 +0700

    fix bug in branch admin/users

--- src/main/java/com/example/bookstore/dao/UsersRepository.java
+++ src/main/java/com/example/bookstore/dao/UsersRepository.java
@@ -2,20 +2,16 @@ package com.example.bookstore.dao;
 
 import com.example.bookstore.entity.Role;
 import com.example.bookstore.entity.User;
-import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import java.util.List;
 
 
 @Repository
-@EnableTransactionManagement
-@Transactional
 public interface UsersRepository extends JpaRepository<User, Integer> {
      User findById(int id);
      User findByUsername(String username);
