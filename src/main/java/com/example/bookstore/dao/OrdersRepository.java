06860a0bde69182d26d2ec9af172512f15055a1c
lenhung1107 <tink60chuyenbienhoa@gmail.com>
2024-09-10 14:21:29 +0700

    view order detail

--- src/main/java/com/example/bookstore/dao/OrdersRepository.java
+++ src/main/java/com/example/bookstore/dao/OrdersRepository.java
@@ -1,6 +1,7 @@
 package com.example.bookstore.dao;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -13,8 +14,11 @@ import com.example.bookstore.entity.Order.Status;
 public interface OrdersRepository extends JpaRepository<Order, Integer> {
 
 	@Query("SELECT o FROM Order o JOIN FETCH o.orderDetails od JOIN FETCH od.book WHERE o.user.id = :userId")
-    List<Order> findOrdersByUserId(Integer userId);
+	List<Order> findOrdersByUserId(Integer userId);
 
 	@Query("SELECT o FROM Order o JOIN FETCH o.orderDetails od JOIN FETCH od.book WHERE o.user.id = :userId AND o.status = :status")
-    List<Order> findOrdersByUserIdAndStatus(Integer userId, Status status);
+	List<Order> findOrdersByUserIdAndStatus(Integer userId, Status status);
+
+	@Query("SELECT o FROM Order o JOIN FETCH o.orderDetails od JOIN FETCH od.book WHERE o.id = :orderId")
+	Optional<Order> findOrderByIdWithDetails(Integer orderId);
 }
--- src/main/java/com/example/bookstore/service/impl/OrdersServiceImpl.java
+++ src/main/java/com/example/bookstore/service/impl/OrdersServiceImpl.java
@@ -17,6 +17,7 @@ import com.example.bookstore.entity.Order;
 import com.example.bookstore.entity.Order.Status;
 import com.example.bookstore.entity.OrderDetail;
 import com.example.bookstore.entity.User;
+import com.example.bookstore.exception.OrderNotFoundException;
 import com.example.bookstore.service.OrdersService;
 
 @Service
@@ -24,48 +25,54 @@ public class OrdersServiceImpl implements OrdersService {
 
 	@Autowired
 	private OrdersRepository ordersRepository;
-	
+
 	@Autowired
 	private OrdersDetailsRepository orderDetailsRepository;
-	
-	@Autowired
-    private CartsRepository cartsRepository;
 
-    @Autowired
-    private CartDetailsRepository cartDetailsRepository;
+	@Autowired
+	private CartsRepository cartsRepository;
+
+	@Autowired
+	private CartDetailsRepository cartDetailsRepository;
 
 	@Override
 	@Transactional
-    public Order saveOrderWithDetails(Order order, List<OrderDetail> orderDetails, User user) {
-        ordersRepository.save(order);
-        
-        for (OrderDetail orderDetail : orderDetails) {
-            orderDetail.setOrder(order); 
-            orderDetailsRepository.save(orderDetail);
-        }
-        
-        Cart cart=cartsRepository.findByUserId(user.getId());
-       	if(cart !=null) {
-           	cartDetailsRepository.deleteByCartId(cart.getId());
-       	}
+	public Order saveOrderWithDetails(Order order, List<OrderDetail> orderDetails, User user) {
+		ordersRepository.save(order);
 
-        return order;
-    }
+		for (OrderDetail orderDetail : orderDetails) {
+			orderDetail.setOrder(order);
+			orderDetailsRepository.save(orderDetail);
+		}
+
+		Cart cart = cartsRepository.findByUserId(user.getId());
+		if (cart != null) {
+			cartDetailsRepository.deleteByCartId(cart.getId());
+		}
+
+		return order;
+	}
 
 	@Override
 	public List<Order> findOrdersByUserId(Integer userId) {
 		List<Order> orders = ordersRepository.findOrdersByUserId(userId);
-        return orders;
+		return orders;
 	}
-
 
 	@Override
 	public List<Order> findOrdersByUserIdAndStatus(Integer userId, Status status) {
 		List<Order> orders = ordersRepository.findOrdersByUserIdAndStatus(userId, status);
 		return orders;
 	}
+
 	@Override
 	public Page<Order> getAll(Pageable pageable) {
 		return ordersRepository.findAll(pageable);
+	}
+
+	@Override
+	public Order findOrderByIdWithDetails(Integer orderId) {
+		return ordersRepository.findOrderByIdWithDetails(orderId)
+				.orElseThrow(() -> new OrderNotFoundException("Order not found with id" + orderId));
 	}
 }
